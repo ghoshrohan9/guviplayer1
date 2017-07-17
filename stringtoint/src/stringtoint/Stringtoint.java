0c35e787e1481b18fe132c08bbe4c72e789bf28d
ghoshrohan9 <rohfriend@gmail.com>
2017-07-17 10:37:11 +0530

    string to int

---
+++ stringtoint/src/stringtoint/Stringtoint.java
@@ -0,0 +1,17 @@
+package stringtoint;
+/**
+ * @author Rohan Ghosh
+ */
+import java.util.*;
+public class Stringtoint {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int b=Integer.parseInt(s);
+        System.out.print(b);
+    }
+    
+}
